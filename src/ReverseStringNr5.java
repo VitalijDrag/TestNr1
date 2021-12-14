27a7222174cbd0eb22e447f163d551eb7c00dc15
VitalijDrag <81467777+VitalijDrag@users.noreply.github.com>
2021-12-14 19:34:07 +0200

    Update ReverseStringNr5.java

--- src/ReverseStringNr5.java
+++ src/ReverseStringNr5.java
@@ -2,8 +2,8 @@ public class ReverseStringNr5 {
     public static void main(String[] args)
     {
         String str = "Will_I_Learn_It";
-
         StringBuffer sbr = new StringBuffer(str);
+        
         sbr.reverse();
         System.out.println(sbr);
     }
